8e18001b473aa5799305cb8b493d110612cfa965
redsteve <redsteve@localhost.localdomain>
2023-09-22 12:09:11 +0300

    Commit by Red-stevo

---
+++ GA_class.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
+class Daro
+{
+	public static void main(String[] args)
+	{
+		//variables
+		double weight, height, bmi;
+		Scanner input = new Scanner(System.in);
+
+		//Ask the user to enter their weigth.
+		System.out.print("Enter your weight: ");
+		weight = input.nextDouble();
+
+		//Ask the user to enter the height
+		System.out.print("Enter your height:");
+		height = input.nextDouble();
+
+		//claculating the bmi
+		bmi = weight/(height * height);
+
+		//displaying the results to the user.
+		System.out.println("Your BMI is: "+bmi);
+		input.close();
+	}
+}
